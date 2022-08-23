1015dd9638ef67bf1f2d50a7d302284eef6ff714
SamuelGlover <sammyglo3@outlook.com>
2022-08-23 21:42:04 +0000

    python tutorial start

---
+++ python/PyFastTutorial.py
@@ -0,0 +1,45 @@
+#Python as fast as possible by tech with tim on youtube
+#Link: https://youtu.be/VchuKL44s6E
+
+#Python is used for: general purpose
+#py is very easy to learn, versatile, and easy to write
+#Core advantage(s): simple, ez syntax, fast dev
+
+#Core Data Types:
+#Int- Any whole number with no decimal
+#Float- Any number with a decimal
+#String- Any quoted text of a sequence of characters
+#-Single vs double quotes doesn't matter, just be consistent
+#to embed quotes use opposite quote type to wrap
+# Boolean- one of two values. true or false, 1 or 0
+
+#------------------------------------------------------------
+#These are your 4 core data types
+
+#Output & Printing
+
+Print("Hello World")
+#Print is the simple function to output to a console on run
+#Hello World is the string
+#you can only print data types.
+#You can pass arguments into Print functions
+
+Print("Hello", "end", 87, False, end="\n" )
+Print("Hello")
+
+#"\n" moves you to the next line
+
+#Lets talk variables
+#In python they're easy to create:
+#variable name + space + equal sign + some value.
+#Variables store information for recall at a later time.
+
+hello = "sam"
+world = "world"
+
+print(hello, world)
+
+#Variable naming conventions:
+#1) no special characters except underscore
+#2) cannot start with a number
+#3) in python underscores typically denote spaces
